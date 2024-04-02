6ce14521d5bc0d79078e9acc3e8de2e877749e86
jaoRBT <joaorosada@gmail.com>
2024-04-02 14:15:37 -0300

    02/04

--- models/Lanches.js
+++ models/Lanches.js
@@ -2,17 +2,9 @@ const Sequelize = require('sequelize')
 const db = require('./../bancoDeDados/conexao')
 
 const Lanches = db.define('lanche', {
-    id:{
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    
     nome: {
         type: Sequelize.STRING
     }
-    
-
-});
+})
 
 module.exports = Lanches
